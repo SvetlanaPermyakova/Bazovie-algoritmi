d3722a01f1177a64e42bdaddece0541446c34882
Roman Permyakov <permyakovr1984@gmail.com>
2023-09-09 22:12:11 +0700

    Добавьте файлы проекта.

---
+++ Bazovie algoritmi/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Numerics;
+
+namespace Bazovie_algoritmi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите координаты первой точки");
+            int xa = Convert.ToInt32(Console.ReadLine());
+            int ya = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите координаты второй точки");
+            int xb = Convert.ToInt32(Console.ReadLine());
+            int yb = Convert.ToInt32(Console.ReadLine());
+
+            double d = Math.Sqrt(Math.Pow(xb - xa, 2) + Math.Pow(yb - ya, 2));
+            Console.WriteLine("Расстояние между точками равно " + d);
+
+
+        }
+    }
+}
